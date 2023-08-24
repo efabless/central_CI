61a3b985b04c13cf4832f2ddb7c6b6a24a2fe900
marwaneltoukhy <marwaneltoukhy@aucegypt.edu>
2023-08-24 13:49:58 +0300

    trial to reduce size

--- .github/scripts/install_dep.py
+++ .github/scripts/install_dep.py
@@ -4,6 +4,7 @@ import argparse
 import requests
 import tarfile
 import os
+import subprocess
 
 
 def get_tool_data(json_file):
@@ -17,24 +18,9 @@ def install_tool(tool, version, url, tool_path):
     if tool == "pdk":
         volare.enable(f'{tool_path}/pdk', "sky130", version)
     else:
-        get_tarball(f'{url}/tarball/{version}', tool)
-        extract_tarball(tool, f'{tool_path}')
-
-
-def get_tarball(url, tool):
-    response = requests.get(url, stream=True)
-    if response.status_code != 200:
-        raise Exception('Failed to get tarball: {}'.format(response.status_code))
-
-    with open(f'{tool}.tar.gz', 'wb') as f:
-        for chunk in response.iter_content(chunk_size=1024):
-            f.write(chunk)
-
-
-def extract_tarball(tool, tool_path):
-    with tarfile.open(f'{tool}.tar.gz', 'r:gz') as tar:
-        tar.extractall(tool_path)
-    os.remove(f'{tool}.tar.gz')
+        ps = subprocess.Popen(['curl', '-L', f'{url}/tarball/{version}'], cwd=tool_path, stdout=subprocess.PIPE)
+        subprocess.check_output(['tar', '-xvzC', '.', '--strip-components=1'], cwd=tool_path, stdin=ps.stdout)
+        ps.wait()
 
 
 def main():
